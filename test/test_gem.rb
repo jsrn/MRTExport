367d2f5c162f2ad70066d1bd09f3792c8eac77cf
jsrn <james.srn@gmail.com>
2014-07-04 14:44:06 +0100

    use relative path

--- test/test_gem.rb
+++ test/test_gem.rb
@@ -3,8 +3,8 @@
 require 'mrtexport'
 
 MRTExport.new({
-  :report_file   => "test_reports/JNCExample.mrt",
-  :output_file   => "test_output/gem_out.pdf",
+  :report_file   => "#{File.dirname(__FILE__)}/test_reports/JNCExample.mrt",
+  :output_file   => "#{File.dirname(__FILE__)}/test_output/gem_out.pdf",
   :export_format => "pdf",
   :replacements  => {"QuoteID" => "1"}
   })
