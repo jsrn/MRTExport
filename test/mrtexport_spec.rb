278e0eee724bcddce0a6ef1969ce4f4e31bf37fc
jsrn <james.srn@gmail.com>
2014-12-08 16:58:24 +0000

    extra test, renamed folder

--- test/mrtexport_spec.rb
+++ test/mrtexport_spec.rb
@@ -1,12 +1,20 @@
 require_relative '../lib/mrtexport.rb'
 
 describe MRTExport, "#score" do
-  it "returns 0 for all gutter game" do
+  it "returns expected output for complex report" do
     MRTExport.new({
       :report_file   => "#{File.dirname(__FILE__)}/test_reports/JNCExample.mrt",
-      :output_file   => "#{File.dirname(__FILE__)}/test_output/out.pdf",
+      :output_file   => "#{File.dirname(__FILE__)}/output/complex_out.pdf",
       :export_format => "pdf",
       :replacements  => {"QuoteID" => "1"}
+    })
+  end
+
+  it "returns expected output for simple report" do
+    MRTExport.new({
+      :report_file   => "#{File.dirname(__FILE__)}/test_reports/a.mrt",
+      :output_file   => "#{File.dirname(__FILE__)}/output/simple_out.pdf",
+      :export_format => "pdf"
     })
   end
 end
