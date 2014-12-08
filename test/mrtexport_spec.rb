4c096338ff85799dae6f555d464a0dc5a586b706
jsrn <james.srn@gmail.com>
2014-12-08 16:55:54 +0000

    tidied test

--- test/mrtexport_spec.rb
+++ test/mrtexport_spec.rb
@@ -1,13 +1,12 @@
-require 'mrtexport'
+require_relative '../lib/mrtexport.rb'
 
 describe MRTExport, "#score" do
   it "returns 0 for all gutter game" do
     MRTExport.new({
       :report_file   => "#{File.dirname(__FILE__)}/test_reports/JNCExample.mrt",
-      :output_file   => "#{File.dirname(__FILE__)}/test_output/gem_out.pdf",
+      :output_file   => "#{File.dirname(__FILE__)}/test_output/out.pdf",
       :export_format => "pdf",
       :replacements  => {"QuoteID" => "1"}
     })
-    should be_true
   end
 end
